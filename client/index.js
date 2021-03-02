21968879545f61a8558efa028a1c752f038749c9
ryanchuah <zcabrwy@ucl.ac.uk>
2021-03-02 18:49:43 +0800

    render movies in client with sort and page limit functionality

--- client/index.js
+++ client/index.js
@@ -1,7 +1,77 @@
 const color = document.getElementById("color");
 
+let sortByElement = document.getElementById("sortBy");
+const moviesTableBody = document.getElementById("moviesTableBody");
+
+sortByElement.onchange = function () {
+	const sortByValue = sortByElement.value;
+	updatePage(sortByValue);
+	window.localStorage.setItem("sortByValue", JSON.stringify(sortByValue));
+};
+
+async function updateDisplay(sortByValue, limit) {
+	moviesTableBody.innerHTML = "";
+	let url =
+		"http://localhost:5000?limit=" +
+		limit +
+		"&sortBy=" +
+		(sortByValue == "default" ? "movieId" : sortByValue);
+	console.log(url);
+
+	const response = await fetch(url);
+
+	if (!response.ok) {
+		console.error(
+			"Error response from server. Error code: ",
+			response.status
+		);
+	} else {
+		const data = await response.json();
+
+		for (const movie of data.movies) {
+			let tr = document.createElement("tr");
+
+			let movieId = document.createElement("td");
+			movieId.innerHTML = movie[0];
+
+			let movieName = document.createElement("td");
+			movieName.innerHTML = movie[1];
+
+			tr.appendChild(movieId);
+			tr.appendChild(movieName);
+			moviesTableBody.appendChild(tr);
+		}
+	}
+}
+
+function updatePage(sortByValue, limit) {
+	if (!sortByValue) {
+		sortByValue = window.localStorage.getItem("sortByValue")
+			? JSON.parse(window.localStorage.getItem("sortByValue"))
+			: "default";
+	}
+	// update "selected" attribute in sortBy HTML element
+	document.querySelector(
+		'#sortBy [value="' + sortByValue + '"]'
+	).selected = true;
+
+	if (!limit) {
+		limit = window.localStorage.getItem("limit")
+			? JSON.parse(window.localStorage.getItem("limit"))
+			: 10;
+	} else {
+		window.localStorage.setItem("limit", JSON.stringify(limit));
+	}
+
+	updateDisplay(sortByValue, limit);
+}
+
+updatePage();
+
+// tutorial function. This function is not actually used anywhere
+// it's just meant to give a feel for JS
 async function insertColor() {
-    const response = await fetch("http://localhost:5000/");
+	const response = await fetch("http://localhost:5000/");
 	if (!response.ok) {
 		console.error(
 			"Error response from server. Error code: ",
@@ -11,28 +81,31 @@ async function insertColor() {
 		const data = await response.json();
 
 		const fav_colors = data.favorite_colors;
-		
+
 		// fav_colors is an array of objects:
 		console.log(fav_colors); // [{Zahra: "blue"}, {Chak: "yellow"}]
-		
+
 		let tag;
 		let text;
-		
-		for(color_obj of fav_colors){ // iterate through array
+
+		for (color_obj of fav_colors) {
+			// iterate through array
 			console.log(color_obj); // {Zahra: "blue"}
 			// iterate through object
-			for(const user in color_obj){
+			for (const user in color_obj) {
 				console.log(user); // Zahra
-				
+
 				tag = document.createElement("li");
-				
-				text = document.createTextNode(`${user} likes ${color_obj[user]}`);
+
+				text = document.createTextNode(
+					`${user} likes ${color_obj[user]}`
+				);
 				tag.appendChild(text);
-				
+
 				color.appendChild(tag);
 			}
 		}
 	}
 }
 
-insertColor();
+// insertColor();
--- server/server.py
+++ server/server.py
@@ -1,39 +1,65 @@
+import json
+from mysql.connector import errorcode
+import mysql.connector
 from flask import Flask
+from typing import List, Dict
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
 # cors settings needed to prevent CORS errors when testing on localhost
 # this should be removed when we deploy to production servers
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'  
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-from typing import List, Dict
-from flask import Flask
-import mysql.connector
-from mysql.connector import errorcode
-import json
 
 app = Flask(__name__)
 
+config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'db',
+    'port': '3306',
+    'database': 'movies_db'
+}
 
-def movies() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'movies_db'
-    }
-    results = None
+
+@app.route("/")
+@cross_origin()
+def index():
+    sortBy = request.args.get('sortBy')
+    Movies_columns = ['movieId', 'title', 'imdbId', 'tmdbId']
+
+    if sortBy and sortBy not in Movies_columns:
+        sortBy = None
+        raise ValueError(
+            f"the request query sortby={sortBy} is not recognized. Either developer error, or SQL injection attempt")
+
+    limit = request.args.get('limit')
+    if limit:
+        try:
+            limit = int(limit)
+        except ValueError:
+            raise ValueError(
+                f"the request query limit={limit} is not recognized. Either developer error, or SQL injection attempt")
+    else:
+        limit = 10
     try:
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM Movies")
-        results = []
+
+        if sortBy:
+            query = (f"SELECT * FROM Movies ORDER BY {sortBy} LIMIT %s")
+            cursor.execute(query, (limit,))
+        else:
+            query = ("SELECT * FROM Movies LIMIT %s")
+            cursor.execute(query, (limit,))
+
+        movies = []
         for movieId, movieTitle, imdbId, tmdbId in cursor:
-            results.append((movieId, movieTitle, imdbId, tmdbId))
+            movies.append((movieId, movieTitle, imdbId, tmdbId))
         cursor.close()
-        return results
+        return {"movies": movies}
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
@@ -44,19 +70,6 @@ def movies() -> List[Dict]:
     finally:
         connection.close()
 
-    # connection = mysql.connector.connect(**config)
-    # cursor = connection.cursor()
-    # cursor.execute('SELECT * FROM Movies')
-    # results = [{movieId: title} for (movieId, title, _, _) in cursor]
-    # cursor.close()
-    # connection.close()
-
-    return results
-
-@app.route("/")
-@cross_origin()
-def index():
-    return json.dumps({'movies': movies()})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
