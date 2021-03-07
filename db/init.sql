029da3aa837ba75bc75d02fe46333181faef1eb9
ryanchuah <zcabrwy@ucl.ac.uk>
2021-03-07 22:49:38 +0800

    add Actor and Actor Roles tables

--- db/init.sql
+++ db/init.sql
@@ -81,30 +81,30 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 
--- CREATE TABLE Actors (
---   actorId VARCHAR(40) not null,
---   actorName VARCHAR(255) not null,
---   PRIMARY KEY (actorId)
--- );
+CREATE TABLE Actors (
+  actorId VARCHAR(40) not null,
+  actorName VARCHAR(255) not null,
+  PRIMARY KEY (actorId)
+);
 
--- LOAD DATA INFILE '/var/lib/mysql-files/datasets/Actors.csv'
--- INTO TABLE Actors
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/datasets/Actors.csv'
+INTO TABLE Actors
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
--- CREATE TABLE Actor_Roles (
---   actorId VARCHAR(40) not null,
---   movieId INT not null,
---   PRIMARY KEY (actorId, movieId),
---   FOREIGN KEY (movieId) REFERENCES Movies(movieId),
---   FOREIGN KEY (actorId) REFERENCES Actors(actorId)
--- );
+CREATE TABLE Actor_Roles (
+  actorId VARCHAR(40) not null,
+  movieId INT not null,
+  PRIMARY KEY (actorId, movieId),
+  FOREIGN KEY (movieId) REFERENCES Movies(movieId),
+  FOREIGN KEY (actorId) REFERENCES Actors(actorId)
+);
 
--- LOAD DATA INFILE '/var/lib/mysql-files/datasets/Actor_Roles.csv'
--- INTO TABLE Tags
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/datasets/Actor_Roles.csv'
+INTO TABLE Actor_Roles
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
