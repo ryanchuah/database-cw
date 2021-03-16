d0c047c3fd52a3738650abab9b9f4e741246bdc5
ryanchuah <zcabrwy@ucl.ac.uk>
2021-03-16 14:33:07 +0800

    add movie header

--- client/navbar.js
+++ client/navbar.js
@@ -1,7 +1,18 @@
-const navbar = document.getElementById("navbar");
+let navbar;
+let root;
+
+if (document.getElementById("navbar")) {
+	navbar = document.getElementById("navbar");
+	root = ".";
+} else {
+	navbar = document.getElementById("navbar-movie");
+	root = "..";
+}
+console.log(window.location.pathname);
+
 navbar.innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="index.html">Home</a>
+	<a class="navbar-brand" href="${root}/index.html">Home</a>
 	<button
 		class="navbar-toggler"
 		type="button"
@@ -16,76 +27,13 @@ navbar.innerHTML = `
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
-            <!--<li class="nav-item active">
-				<a class="nav-link" href="index.html"
-					>Movies <span class="sr-only">(current)</span></a
-				>
-			</li> 
-
-			<li class="nav-item dropdown">
-				<a
-					class="nav-link dropdown-toggle"
-					href="#"
-					id="navbarDropdown"
-					role="button"
-					data-toggle="dropdown"
-					aria-haspopup="true"
-					aria-expanded="false"
-				>
-					Genres
-				</a>
-				<div
-					class="dropdown-menu dropdown-multicol"
-					aria-labelledby="dropdownMenuButton"
-				>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Action</a
-						><a class="dropdown-item" href="#">Adventure</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Animation</a
-						><a class="dropdown-item" href="#">Children</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Comedy</a
-						><a class="dropdown-item" href="#">Crime</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Documentary</a
-						><a class="dropdown-item" href="#">Drama</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Fantasy</a
-						><a class="dropdown-item" href="#">Film-Noir</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Horror</a
-						><a class="dropdown-item" href="#">IMAX</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Musical</a
-						><a class="dropdown-item" href="#">Mystery</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Romance</a
-						><a class="dropdown-item" href="#">Sci-Fi</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Thriller</a
-						><a class="dropdown-item" href="#">War</a>
-					</div>
-					<div class="dropdown-row">
-						<a class="dropdown-item" href="#">Western</a>
-					</div>
-				</div>
-			</li> -->   
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Predict
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="predict-ratings.html">Ratings</a>
-                    <a class="dropdown-item" href="predict-personality.html">Personality Traits</a>
+                    <a class="dropdown-item" href="${root}/predict-ratings.html">Ratings</a>
+                    <a class="dropdown-item" href="${root}/predict-personality.html">Personality Traits</a>
                 </div>
             </li>
 			
@@ -117,7 +65,7 @@ function handleAllMovies() {
 	);
 	window.localStorage.setItem("search-limit", JSON.stringify(10));
 	window.localStorage.setItem("search-page", JSON.stringify(1));
-	window.location.href = "search.html";
+	window.location.href = `${root}/search.html`;
 }
 async function searchMovie(event) {
 	event.preventDefault();
@@ -126,7 +74,7 @@ async function searchMovie(event) {
 		"search-searchTerm",
 		JSON.stringify(searchTerm)
 	);
-	window.location.href = "search.html";
+	window.location.href = `${root}/search.html`;
 }
 
 const form = document.getElementById("search");
